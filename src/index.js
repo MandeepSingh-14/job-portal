94346a6c75c8d7febfaa49e6fa2774dfb0d0e16e
mandeep singh <mandeep14042002@gmail.com>
2024-09-04 11:38:38 +0530

    application logic completed

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import connectDB from "./db/index.js";
 import userRoute from "./routes/user.routes.js";
 import companyRoute from "./routes/company.routes.js";   
 import jobRoute from "./routes/job.routes.js"
+import applicationRoute from "./routes/application.routes.js"
+
 
 const app = express();
 app.use(express.json({limit:"16kb"}))
@@ -28,6 +30,7 @@ const PORT = process.env.PORT||3000 ;
 app.use("/api/v1/users",userRoute)
 app.use("/api/v1/companies",companyRoute)
 app.use("/api/v1/jobs",jobRoute)
+app.use("/api/v1/applications",applicationRoute)
 
 app.listen(PORT, () => {
   connectDB();
--- src/routes/application.routes.js
+++ src/routes/application.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
-import { applyJob } from "../controllers/application.controller.js";
+import { applyJob, findApplicants, getAppliedJobs, updateStatus } from "../controllers/application.controller.js";
 const router = Router();
 
-router.route("/apply").post(verifyJWT,applyJob);
-router.route("get").get(verifyJWT,)
+router.route("/apply/:id").get(verifyJWT,applyJob);
+router.route("/get").get(verifyJWT,getAppliedJobs);
+router.route("/:id/applicants").get(verifyJWT,findApplicants);//The :id parameter represents the unique identifier of the job for which you want to find the applicants. This is how you tell the server which specific job you are referring to. 
+router.route("/status/:id/update").post(verifyJWT,updateStatus); //The structure /status/:id/update clearly conveys that the request is meant to update the status of a specific job application.
+
+
+export default router
